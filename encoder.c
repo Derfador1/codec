4a0bcdcc941b00f8bf3835c8e6ad1691c0b22136
Stephen Paradis <sparadis@localhost.localdomain>
2015-11-19 13:54:35 -0500

    Added ability to read values to storein variables and then ability to encode hex values

--- encoder.c
+++ encoder.c
@@ -2,74 +2,107 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
 #define SIZE 255
 
-void dec_to_hex(unsigned int value, char * buf);
 
 struct meditrik {
-	//check mask
-	unsigned int version : 4;
-	unsigned int seq_id : 9;
 	unsigned int type : 3;
+	unsigned int seq_id : 9;
+	unsigned int version : 4;
 	unsigned int total_length : 16;
 	unsigned int source_device_id : 32;
 	unsigned int dest_device_id : 32;
-
 };
 
 union bytes {
 	struct meditrik medi;
-	unsigned short data;	
+	unsigned short data[6];
+	unsigned int data2[3];
 };
 
 
-int main(void)
+int main(int argc, char * argv[])
 {
-	int j;
-
-	char *buf;
-
-	buf = malloc(13);
-
-	memset(buf, '\0', 13);
+	if (argc == 1)
+	{
+		printf("whore\n");
+	}
 
 	union bytes byte;
 
-	byte.data = 0;
+	memset(byte.data2, '\0', sizeof(byte.data2));
 
-	byte.medi.version = 1;	
-	byte.medi.seq_id = 81;
-	byte.medi.type = 1;
-	byte.medi.total_length = 18;
-	byte.medi.source_device_id = 7890;
-	byte.medi.dest_device_id = 1234;
-	
+	FILE *reader;
+	int version = 0;
+	int seq_id = 0;
+	int type = 0;
+	int total_length = 0;
+	int source_device_id = 0;
+	int dest_device_id = 0;
 
-	dec_to_hex(byte.medi.source_device_id, buf);
+	reader = fopen("decoded.txt", "r");
 
-	FILE *fp;
+	fscanf(reader, "Version: %d\n", &version);
+	fscanf(reader, "Sequence: %d\n", &seq_id);
+	fscanf(reader, "Type: %d\n", &type);
+	fscanf(reader, "Total Length: %d\n", &total_length);
+	fscanf(reader, "Source Device: %d\n", &source_device_id);
+	fscanf(reader, "Destination Device: %d\n", &dest_device_id);
 
-	fp = fopen("test.pcap", "w+");
+	byte.medi.version = version;	
+	byte.medi.seq_id = seq_id;
+	byte.medi.type = type;
+	byte.medi.total_length = total_length;
+	byte.medi.source_device_id = source_device_id;
+	byte.medi.dest_device_id = dest_device_id;
 
-	for (j = 0; j < 12; j++)
+	printf("%d\n", version);
+	printf("%d\n", seq_id);
+	printf("%d\n", type);
+	printf("%d\n", total_length);
+	printf("%d\n", source_device_id);
+	printf("%d\n", dest_device_id);
+
+	FILE *writer;
+
+	writer = fopen(argv[1], "w+");
+
+
+	char *fake_buffer = malloc(1);
+
+	fake_buffer[0] = 0;
+
+	byte.data[0] = htons(byte.data[0]);
+	byte.data[1] = htons(byte.data[1]);
+
+	byte.data2[1] = htonl(byte.data2[1]);
+	byte.data2[2] = htonl(byte.data2[2]);
+
+
+	size_t one = 1;
+
+	size_t three = 3;
+
+	for (int c = 0; c < 82; c++)
 	{
-		fprintf(fp ,"%c", buf[j]);
+		fwrite(fake_buffer, 1, one, writer);
 	}
 
-	/*
-	for (j = 0; j < 12; j++)
-	{
-		printf("%c", buf[j]);
-	}
-	*/
+	fwrite(&byte.data2, 4, three, writer);
 
-	fclose(fp);
+	free(fake_buffer);
 
-	free(buf);
+	fclose(writer);
+
+	fclose(reader);
 	
 }
 
+
+
 //convert to string and print out string with for loop buf[i]
+/*
 void dec_to_hex(unsigned int value, char * buf)
 {
 	//long int remainder;
@@ -82,12 +115,12 @@ void dec_to_hex(unsigned int value, char * buf)
 	{
 		temp = quotient % 16;
 
-		/*
+
 		if(temp < 10)
 			temp = temp + 48;
 		else
 			temp = temp + 55;
-		*/
+
 
 		buf[i++] = temp;
 
@@ -97,3 +130,4 @@ void dec_to_hex(unsigned int value, char * buf)
 	//return 0;
 	
 }
+*/
