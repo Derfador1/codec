8551cb8bce571e0787b17e5fd7895024e0bc2df1
Stephen Paradis <sparadis@localhost.localdomain>
2015-11-23 10:37:17 -0500

    Fix #9, added the rest fo the fields to be encoded for both gps and status

--- encoder.c
+++ encoder.c
@@ -21,6 +21,13 @@ struct gps {
 	float alt;
 };
 
+struct status {
+	double battery;
+	short glucose;
+	short capsaicin;
+	short omorfine;
+};
+
 union gps_header{
 	struct gps fields;
 	unsigned char degrees[20];
@@ -32,12 +39,20 @@ union bytes {
 	unsigned int data2[3];
 };
 
+union stat_payload {
+	struct status payload;
+	unsigned short printer[7];
+};
+
 int fill(char * fake_buffer, size_t one, FILE *writer);
-int get_value(char * x, union bytes *byte, union gps_header *info);
+int get_value(char * x, union bytes *byte, union gps_header *info, union stat_payload *packet, unsigned int *type_pt);
+//int get_gps(char * x, union gps_header *info);
+//int get_statpayload(char *x, union stat_playload *packet);
 
 int main(int argc, char * argv[])
 {
 	char * x;
+	unsigned int *type_pt = malloc(sizeof(type_pt));
 	if (argc == 1)
 	{
 		printf("Please retry with a valid file to open.\n");
@@ -49,17 +64,21 @@ int main(int argc, char * argv[])
 	}
 
 	x = argv[1];
+
 	union gps_header info;
 	union bytes byte;
+	union stat_payload packet;
 
 	memset(byte.data2, '\0', sizeof(byte.data2));
+	memset(byte.data, '\0', sizeof(byte.data));
+	memset(info.degrees, '\0', sizeof(info.degrees));
+	memset(packet.printer, '\0', sizeof(packet.printer));
 
-	get_value(x, &byte, &info);
+	get_value(x, &byte, &info, &packet, type_pt);
 
 	FILE *writer;
 
 	writer = fopen(argv[2], "w+");
-
 
 	char *fake_buffer = malloc(1);
 
@@ -70,6 +89,10 @@ int main(int argc, char * argv[])
 	byte.data2[1] = htonl(byte.data2[1]);
 	byte.data2[2] = htonl(byte.data2[2]);
 
+	//move to if statement
+	packet.printer[4] = htons(packet.printer[4]);
+	packet.printer[5] = htons(packet.printer[5]);
+	packet.printer[6] = htons(packet.printer[6]);
 
 	size_t one = 1;
 
@@ -79,9 +102,18 @@ int main(int argc, char * argv[])
 
 	fwrite(&byte.data, 2, six, writer);
 
-	fwrite(&info.degrees, 20, one, writer);
+	if (*type_pt == 0)
+	{
+		fwrite(&packet.printer, 2, 7, writer); //sizeof(packet.fields) instead of 14
+	}
+	else if (*type_pt == 2)
+	{
+		fwrite(&info.degrees, 20, one, writer);
+	}
 
 	free(fake_buffer);
+
+	free(type_pt);
 
 	fclose(writer);
 }
@@ -97,7 +129,7 @@ int fill(char * fake_buffer, size_t one, FILE *writer)
 }
 
 
-int get_value(char * x, union bytes *byte, union gps_header *info)
+int get_value(char * x, union bytes *byte, union gps_header *info, union stat_payload *packet, unsigned int *type_pt)
 {
 	FILE *reader;
 	reader = fopen(x, "r");
@@ -123,19 +155,57 @@ int get_value(char * x, union bytes *byte, union gps_header *info)
 	(*byte).medi.source_device_id = *source_device_id;
 	(*byte).medi.dest_device_id = *dest_device_id;
 
-	if (*type == 2)
+	*type_pt = *type;
+
+	if (*type == 0)
+	{
+		double *power = malloc(sizeof(double));
+		short *glucose = malloc(sizeof(short));
+		short *capsaicin = malloc(sizeof(short));
+		short *omorfine = malloc(sizeof(short));
+
+		fscanf(reader, "Battery power : %lf%%\n", power);
+		fscanf(reader, "Glucose: %hd\n", glucose);
+		fscanf(reader, "Capsaicin: %hd\n", capsaicin);
+		fscanf(reader, "Omorfine: %hd\n", omorfine);
+
+		(*packet).payload.battery = (*power/100);
+		(*packet).payload.glucose = *glucose;
+		(*packet).payload.capsaicin = *capsaicin;
+		(*packet).payload.omorfine = *omorfine;
+
+		printf("%.2f\n", *power);
+		printf("%d\n", *glucose);
+		printf("%d\n", *capsaicin);
+		printf("%d\n", *omorfine);
+
+		free(power);
+		free(glucose);
+		free(capsaicin);
+		free(omorfine);
+	}	
+	else if (*type == 2)
 	{
 		double *tude = malloc(sizeof(double));
-		double *longs = malloc(sizeof(double));
-		double *alt = malloc(sizeof(double));
+		double *lon = malloc(sizeof(double));
+		float *alt = malloc(sizeof(float));
 
-		fscanf(reader, "Latitude : %lf\n", tude);
+		fscanf(reader, "Latitude : %lf degree N\n", tude);
+		fscanf(reader, "Longitude : %lf degree W\n", lon);
+		fscanf(reader, "Altitude : %f ft\n", alt);
+
 		(*info).fields.lat = *tude;
+		(*info).fields.longs = *lon;
+		(*info).fields.alt = (*alt/6);
 
+		/*
 		printf("%.9lf\n", *tude);
+		printf("%.9lf\n", *lon);
+		printf("%.1f\n", (*info).fields.alt);
+		*/
 
 		free(tude);
-		free(longs);
+		free(lon);
 		free(alt);
 	}
 
